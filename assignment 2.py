47eba420020e146f21bc004ffc9eee76f9e468fd
pragya62550 <pragyadubey62550@gmail.com>
2022-08-27 19:57:59 +0530

    assignment uploads

---
+++ assignment 2.py
@@ -0,0 +1,93 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================================ RESTART: Shell ================================
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+21 34 56 76
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+Traceback (most recent call last):
+  File "C:\Users\win\py assignment.py", line 2, in <module>
+    b=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+<class 'int'>
+<class 'bool'>
+<class 'str'>
+<class 'float'>
+<class 'complex'>
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+1822216881616
+1822216881616
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+140719988542192
+140719988493520
+140719988497680
+140719988533872
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+<class 'int'>
+<class 'bool'>
+<class 'complex'>
+<class 'float'>
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+help()
+
+Welcome to Python 3.10's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the internet at https://docs.python.org/3.10/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> keywords
+
+Here is a list of the Python keywords.  Enter any keyword to get more help.
+
+False               class               from                or
+None                continue            global              pass
+True                def                 if                  raise
+and                 del                 import              return
+as                  elif                in                  try
+assert              else                is                  while
+async               except              lambda              with
+await               finally             nonlocal            yield
+break               for                 not                 
+
+help> 
+
+You are now leaving help and returning to the Python interpreter.
+If you want to ask for help on a particular object directly from the
+interpreter, you can type "help(object)".  Executing "help('string')"
+has the same effect as typing a particular string at the help> prompt.
+
+========================== RESTART: C:\Users\win\A0.py =========================
+Teacher Day
+
+========================== RESTART: C:\Users\win\A1.py =========================
+
+========================== RESTART: C:\Users\win\A0.py =========================
+30
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+True False None
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+27-08-2022 and 04:36PM
---
+++ assignment first.py
@@ -0,0 +1,42 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)]
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+Hello python
+print("Hello python")
+Hello python
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+Hello
+python
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+"MySirG"
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+Pragya Dubey
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+Traceback (most recent call last):
+  File "C:/Users/win/py assignment.py", line 3, in <module>
+    c=(b.com)
+AttributeError: 'int' object has no attribute 'com'
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+Pragya Dubey 22 bba 1
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+Pragya Dubey
+22
+bba
+1
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+
+==================== RESTART: C:/Users/win/py assignment.py ====================
+58
+86
+85
