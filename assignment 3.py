b6f26bf433be2a765ef4388bf0fe049a8dab3716
pragya62550 <pragyadubey62550@gmail.com>
2022-08-28 18:53:37 +0530

    assignmnent

---
+++ assignment 3.py
@@ -0,0 +1,38 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+<class 'str'>
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+109
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+Traceback (most recent call last):
+  File "C:\Users\win\py assignment.py", line 1, in <module>
+    print(chr('100'))
+TypeError: 'str' object cannot be interpreted as an integer
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+d
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+0b111010
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+0o72
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+0x3a
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+101
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+0o57
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+0b1010101
+
+==================== RESTART: C:\Users\win\py assignment.py ====================
+0b101010b111001
